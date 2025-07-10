7a4f5b13a39ef4dbce2e36861d02b093cb331c33
Thiveran <thivyaprabhakara@gmail.com>
2025-07-10 12:39:30 +0530

    Create india-page.tsx

---
+++ pages/india-page.tsx
@@ -0,0 +1,8 @@
+export default function IndiaPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🇮🇳 Namaste! Welcome to the India page.</h1>
+      <p>This page is customized for visitors from India.</p>
+    </div>
+  );
+}
