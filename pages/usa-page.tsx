5bbc2b5741ca513de9a6e10351fbb70f78db9a16
Thiveran <thivyaprabhakara@gmail.com>
2025-07-10 13:03:29 +0530

    Create usa-page.tsx

---
+++ pages/usa-page.tsx
@@ -0,0 +1,9 @@
+export default function USAPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🇺🇸 Hello from the USA page!</h1>
+      <p>This page is shown only to visitors from the United States.</p>
+      <h1>PRABHA FROM US</h1>
+    </div>
+  );
+}
