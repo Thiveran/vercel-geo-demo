7a9cdfdc49dac299eca06611392fc1e1ab3d31d2
Thiveran <thivyaprabhakara@gmail.com>
2025-07-10 12:38:29 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🌐 Welcome to the Homepage</h1>
+      <p>This site redirects Indian visitors to a special page.</p>
+    </div>
+  );
+}
